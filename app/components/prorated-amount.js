3ec4838462bc6121ffd0df19feb42c656623f833
matt <mattkleker@gmail.com>
2014-11-20 00:20:51 -0500

    added checkbox to toggle and moved functions to components

---
+++ app/components/prorated-amount.js
@@ -0,0 +1,62 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  today: new Date(),
+
+  // todayMoment: moment(),
+
+  // momentDate: function() {
+  //   return todayMoment;
+  // }.property('todayMoment'),
+
+  momentDate: function() {
+    return moment().format('LL');
+  },
+
+  date: function() {
+    return this.get('today').getDate();
+  }.property('today'),
+
+  daysInMonth: function() {
+
+    var month = this.get('today').getMonth();
+
+    var year = this.get('today').getFullYear();
+
+    return new Date(year,month+1,0).getDate();
+  }.property('year', 'month'),
+
+
+  daysLeftInMonth: function() {
+    return this.get('daysInMonth') - this.get('date') ;
+  }.property('daysInMonth', 'date'),
+
+  prorateFactor: function() {
+    return this.get('daysLeftInMonth') / this.get('daysInMonth');
+  }.property('daysInMonth', 'date'),
+
+  proratedRate: function() {
+    var currentMonthAmount = this.get('monthlyAmount') * this.get('prorateFactor');
+    if (this.get('includeNextMonth')) {
+      return currentMonthAmount + this.get('monthlyAmount');
+    }
+    return currentMonthAmount;
+  }.property('monthlyAmount', 'prorateFactor','includeNextMonth'),
+
+  blurb: function() {
+    var month = this.get('today').getMonth() + 1;
+
+    var year = this.get('today').getFullYear();
+
+    var lastDayOfMonth = new Date(year,month,0).getDate();
+
+    return '  **This ' + this.get('monthlyAmount') + '/month fee is prorated to cover ' + month + '/' + this.get('date') + '/' + year + ' - ' + month + '/' + lastDayOfMonth + '/' + year;
+  }.property('monthlyAmount', 'date'),
+
+  copyBlurb: function() {
+
+  }
+
+
+});
