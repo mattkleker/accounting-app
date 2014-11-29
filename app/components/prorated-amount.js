83a40af6e7dc882fe15c8171bd5dece45b8302ea
matt <mattkleker@gmail.com>
2014-11-29 01:11:50 -0500

    cleaned up prorated amount component

--- app/components/prorated-amount.js
+++ app/components/prorated-amount.js
@@ -1,32 +1,11 @@
 import Ember from 'ember';
-// import { moment } from 'ember-moment/computed';
 
 export default Ember.Component.extend({
 
-  // jsToday: new Date(),
   today: moment(),
   lastDayOfMonth: function() {
     return this.get('today').clone().endOf("month");
   }.property('today'),
-  // todayMoment: moment(),
-
-  // momentDate: moment(),
-  // shortDate: moment('date', 'MM/DD/YYYY'),        
-
-
-  // date: function() {
-  //   return this.get('today').date();
-  // }.property('today'),
-
-  // daysInMonth: function() {
-
-  //   var month = this.get('today').getMonth();
-
-  //   var year = this.get('today').getFullYear();
-
-  //   return new Date(year,month+1,0).getDate();
-  // }.property('year', 'month'),
-
 
   daysLeftInMonth: function() {
     return this.get('lastDayOfMonth').date() - this.get('today').date();
@@ -45,13 +24,7 @@ export default Ember.Component.extend({
   }.property('prorateFactor','includeNextMonth','monthlyAmount'),
 
   blurb: function() {
-    // var month = this.get('today').getMonth() + 1;
-
-    // var year = this.get('today').getFullYear();
-
-    // var lastDayOfMonth = new Date(year,month,0).getDate();
-
-    return '  **This ' + this.get('monthlyAmount') + '/month fee is prorated to cover ' + this.get('today').add(1, "day").format("L") + " - " + this.get('lastDayOfMonth').format("L");
+    return '  **This ' + this.get('monthlyAmount') + '/month fee is prorated to cover ' + this.get('today').clone().add(1, "day").format("L") + " - " + this.get('lastDayOfMonth').format("L");
   }.property('monthlyAmount', 'today', "lastDayOfMonth"),
 
   copyBlurb: function() {
