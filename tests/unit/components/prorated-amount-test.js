ade466c19deca14bd3bf350b9939d8979c687736
Sam Selikoff <sam.selikoff@gmail.com>
2014-12-05 09:16:50 -0500

    Fix prorated amount unit test (closes #7).

--- tests/unit/components/prorated-amount-test.js
+++ tests/unit/components/prorated-amount-test.js
@@ -5,17 +5,17 @@ import {
 } from 'ember-qunit';
 import { initialize } from '../../../initializers/ember-moment';
 
-moduleForComponent('prorated-amount', 'ProratedAmountComponent', 'moment-test', 'component which uses ember-moment helpers', {
+moduleForComponent('prorated-amount', 'ProratedAmountComponent', {
   // specify the other units that are required for this test
-  needs: ['component:zero-clipboard'], 
+  needs: ['component:zero-clipboard'],
   setup: function (container) {
-        Ember.run(function () {
-            // these two arguments are not used
-            // but probably still good to pass them in
-            // in the event we leverage them in the future
-            initialize(container, Ember.Application.create());
-        });
-    }
+    Ember.run(function () {
+      // these two arguments are not used
+      // but probably still good to pass them in
+      // in the event we leverage them in the future
+      initialize(container);
+    });
+  }
 });
 
 test('it renders', function() {
@@ -27,13 +27,15 @@ test('it renders', function() {
 
   // appends the component to the page
   this.append();
-  equal(component._state, 'inDOM');
+  Ember.run(function() {
+    equal(component._state, 'inDOM');
+  });
 });
 
 test('it calculates days left in month when current date is first day of month', function() {
   var component = this.subject( {
     today: moment("2014-10-01")
-  });  
+  });
 
   equal(component.get('daysLeftInMonth'), 30);
 });
@@ -41,7 +43,7 @@ test('it calculates days left in month when current date is first day of month',
 test('it calculates days left in month when current date is last day of month', function() {
   var component = this.subject( {
     today: moment("2014-10-31")
-  });  
+  });
 
   equal(component.get('daysLeftInMonth'), 0);
 });
@@ -65,7 +67,7 @@ test('it calculates prorate factor when current date is mid month', function() {
 test('it calculates prorate factor when current date is last day of month', function() {
   var component = this.subject( {
     today: moment("2014-10-31")
-  });  
+  });
 
   equal(component.get('prorateFactor'), 0);
 });
@@ -74,7 +76,7 @@ test('it calculates the rate properly', function() {
   var component = this.subject( {
     today: moment("2014-11-15"),
     monthlyAmount: 1000
-  });  
+  });
 
   equal(component.get('proratedRate'), 500);
 });
@@ -84,7 +86,7 @@ test('the blurb is correct when the date is the 18th', function() {
     today: moment("2014-11-18"),
     monthlyAmount: 1000,
     includeNextMonth: true
-  });  
+  });
 
   equal(component.get('blurb'), "  **This $1000/month fee is prorated to cover 11/19/2014 - 12/31/2014");
 });
@@ -93,7 +95,7 @@ test('the blurb is correct when the date is the 3rd', function() {
   var component = this.subject( {
     today: moment("2014-11-03"),
     monthlyAmount: 1000
-  });  
+  });
 
   equal(component.get('blurb'), "  **This $1000/month fee is prorated to cover 11/04/2014 - 11/30/2014");
 });
@@ -103,9 +105,7 @@ test('the blurb is correct when the date is the 26th and includeNextMonth is unc
     today: moment("2014-11-26"),
     monthlyAmount: 1000,
     includeNextMonth: false
-  });  
+  });
 
   equal(component.get('blurb'), "  **This $1000/month fee is prorated to cover 11/27/2014 - 11/30/2014");
 });
-
-// 
