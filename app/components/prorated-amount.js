6ac196840d9d0f53d3f88b9198413fef4aa5c4bd
matt <mattkleker@gmail.com>
2015-01-01 22:32:17 -0500

    added functions to prorate rate via start and end dates

--- app/components/prorated-amount.js
+++ app/components/prorated-amount.js
@@ -2,44 +2,80 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  today: moment(),
-  lastDayOfMonth: function() {
-    return this.get('today').clone().endOf("month");
-  }.property('today'),
+  // formattedValue: function() {
+  //   return this.get('monthlyAmount').clone().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  // }.property('monthlyAmount'),
 
-  daysLeftInMonth: function() {
-    return this.get('lastDayOfMonth').date() - this.get('today').date();
-  }.property('lastDayOfMonth', 'today'),
+  // formattedValue: function() {
+  //   var string = numeral(this.get('monthlyAmount').clone()).format('0,0');
+  //   return string;
+  // }.property('monthlyAmount'),
 
-  prorateFactor: function() {
-    return this.get('daysLeftInMonth') / this.get('lastDayOfMonth').date();
-  }.property('daysLeftInMonth', 'lastDayOfMonth'),
+  startDateSetter: function() {
+    this.set('start', moment().add(1,"day").format('MM/DD/YYYY'));
+  }.on('init'),
 
-  prorateEquation: function() {
-      var equation = "Prorated rate: " + this.get('monthlyAmount') + " / " + this.get('lastDayOfMonth').format("D") + " x " + this.get('daysLeftInMonth');
-      if (this.get('includeNextMonth')) {
-        return equation + " + " + this.get('monthlyAmount') + " =";
-      }
-      return equation + " =";
-  }.property('monthlyAmount','lastDayOfMonth','daysLeftInMonth','includeNextMonth'),
+  startDate: function() {
+    return moment(this.get('start'));
+  }.property('start'),
+
+  startDateMonthEnd: function() {
+    return this.get('startDate').clone().endOf("month");
+  }.property('startDate'),
+
+  startDateProrateDays: function() {
+    var monthDiff = this.get('endDate').clone().startOf("month").diff(this.get('startDate').clone().startOf("month"), 'months');
+    if (monthDiff > 0) {
+      return this.get('startDateMonthEnd').diff(this.get('startDate'), 'days') + 1;
+    }
+      return this.get('endDate').diff(this.get('startDate'), 'days') + 1;
+  }.property('startDate', 'startDateMonthEnd', 'endDate'),
+
+  startDateProratedRate: function() {
+    return this.get('monthlyAmount') / this.get('startDateMonthEnd').date() * this.get('startDateProrateDays');
+  }.property('monthlyAmount', 'startDateMonthEnd', 'startDateProrateDays'),
+
+  endDateSetter: function() {
+    if (this.get('startDate').date() > 17) {
+      this.set('end', moment().add(1, "month").endOf("month").format('MM/DD/YYYY'));
+    }
+      this.set('end', moment().endOf("month").format('MM/DD/YYYY'));
+    }.on('init'),  
+
+  endDate: function() {
+    return moment(this.get('end'));
+  }.property('end'),
+
+  endDateMonthStart: function() {
+    return this.get('endDate').clone().startOf("month");
+  }.property('endDate'),
+
+  endDateProrateDays: function() {
+    var monthDiff = this.get('endDate').clone().startOf("month").diff(this.get('startDate').clone().startOf("month"), 'months');
+    if (monthDiff > 0) {
+    return this.get('endDate').diff(this.get('endDateMonthStart'), 'days') + 1;
+    }
+    return 0;
+  }.property('endDate', 'endDateMonthStart'),
+
+  endDateProratedRate: function() {
+    return this.get('monthlyAmount') / this.get('endDate').clone().endOf("month").date() * this.get('endDateProrateDays');
+  }.property('monthlyAmount', 'endDate', 'endDateProrateDays'),
+
+  middleMonthCount: function() {
+    var monthDiff = this.get('endDate').clone().startOf("month").diff(this.get('startDate').clone().startOf("month"), 'months');
+    if (monthDiff > 1) {
+      return monthDiff - 1;
+    }
+    return 0;
+  }.property('startDate', 'endDate'),
 
   proratedRate: function() {
-    var proratedAmount = this.get('monthlyAmount') * this.get('prorateFactor');
-    if (this.get('includeNextMonth')) {
-      var proratedAmountWithNextMonth = proratedAmount + parseInt(this.get('monthlyAmount'), 10);
-      return proratedAmountWithNextMonth.toFixed(2);
-    }
-    return proratedAmount.toFixed(2);
-  }.property('prorateFactor','includeNextMonth','monthlyAmount'),
+    return this.get('startDateProratedRate') + this.get('middleMonthCount') * this.get('monthlyAmount') + this.get('endDateProratedRate');
+  }.property('startDateProratedRate', 'middleMonthCount', 'monthlyAmount', 'endDateProratedRate'),
 
   blurb: function() {
-    var blurb = '  **This $' + this.get('monthlyAmount') + '/month fee is prorated to cover ' + this.get('today').clone().add(1, "day").format("L") + " - ";
-    if (this.get('includeNextMonth')) {
-      blurb += this.get('today').clone().add(1, "month").endOf("month").format("L");
-    } else {
-      blurb += this.get('lastDayOfMonth').format("L");
-    }
-    return blurb;
-  }.property('monthlyAmount', 'today', "lastDayOfMonth","includeNextMonth")
+    return '  **This $' + this.get('monthlyAmount') + '/month fee is prorated to cover ' + this.get('startDate').format("L") + " - " + this.get('endDate').format("L");
+  }.property('monthlyAmount', 'startDate', 'endDate')
 
 });
--- tests/unit/components/prorated-amount-test.js
+++ tests/unit/components/prorated-amount-test.js
@@ -32,80 +32,202 @@ test('it renders', function() {
   });
 });
 
-test('it calculates days left in month when current date is first day of month', function() {
+test('it sets the start date to tomorrow', function() {
   var component = this.subject( {
-    today: moment("2014-10-01")
+
   });
 
-  equal(component.get('daysLeftInMonth'), 30);
+  equal(component.get('startDate').date(), moment().add(1,"days").date());
 });
 
-test('it calculates days left in month when current date is last day of month', function() {
+test('it sets the end date to the end of the month if start date is the first', function() {
   var component = this.subject( {
-    today: moment("2014-10-31")
+    startDate: moment("2015-01-01")
   });
 
-  equal(component.get('daysLeftInMonth'), 0);
+  equal(component.get('endDate').date(), 31);
 });
 
-test('it calculates prorate factor when current date is the first of the month', function() {
+test('it sets the end date to the end of the next month if start date is the 18th', function() {
   var component = this.subject( {
-    today: moment("2014-11-01")
+    startDate: moment("2015-01-18")
   });
 
-  equal(component.get('prorateFactor'), 29/30);
+  equal(component.get('endDate').date(), 28);
 });
 
-test('it calculates prorate factor when current date is mid month', function() {
+test('start date: it calculates days left in month when current date is first day of month', function() {
   var component = this.subject( {
-    today: moment("2014-11-15")
+    startDate: moment("2014-10-01")
   });
 
-  equal(component.get('prorateFactor'), 0.5);
+  equal(component.get('startDateProrateDays'), 31);
 });
 
-test('it calculates prorate factor when current date is last day of month', function() {
+test('start date: it calculates days left in month when current date is last day of month', function() {
   var component = this.subject( {
-    today: moment("2014-10-31")
+    startDate: moment("2014-10-31")
   });
 
-  equal(component.get('prorateFactor'), 0);
+  equal(component.get('startDateProrateDays'), 1);
 });
 
-test('it calculates the rate properly', function() {
+test('end date: it calculates zero days left in month when startDate and endDate are in the same month', function() {
   var component = this.subject( {
-    today: moment("2014-11-15"),
+    startDate: moment("2014-10-02"),
+    endDate: moment("2014-10-03")
+  });
+
+  equal(component.get('endDateProrateDays'), 0);
+});
+
+test('start date: it calculates days left in month when startDate and endDate are in the same month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-10-02"),
+    endDate: moment("2014-10-03")
+  });
+
+  equal(component.get('startDateProrateDays'), 2);
+});
+
+test('end date: it calculates number of days to prorate when end date is first day of month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-10-01")
+  });
+
+  equal(component.get('endDateProrateDays'), 1);
+});
+
+test('end date: it calculates number of days to prorate when end date is last day of month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-10-31")
+  });
+
+  equal(component.get('endDateProrateDays'), 31);
+});
+
+test('start date: it calculates prorated rate when start date is the first of the month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-11-01"),
     monthlyAmount: 1000
   });
 
-  equal(component.get('proratedRate'), 500);
+  equal(Math.round(component.get('startDateProratedRate')), 1000);
 });
 
-test('the blurb is correct when the date is the 18th', function() {
+test('start date: it calculates prorated rate when start date is mid month', function() {
   var component = this.subject( {
-    today: moment("2014-11-18"),
-    monthlyAmount: 1000,
-    includeNextMonth: true
-  });
-
-  equal(component.get('blurb'), "  **This $1000/month fee is prorated to cover 11/19/2014 - 12/31/2014");
-});
-
-test('the blurb is correct when the date is the 3rd', function() {
-  var component = this.subject( {
-    today: moment("2014-11-03"),
+    startDate: moment("2014-11-16"),
     monthlyAmount: 1000
   });
 
-  equal(component.get('blurb'), "  **This $1000/month fee is prorated to cover 11/04/2014 - 11/30/2014");
+  equal(Math.round(component.get('startDateProratedRate')), 500);
 });
 
-test('the blurb is correct when the date is the 26th and includeNextMonth is unchecked', function() {
+test('start date: it calculates prorated rate when start date is last day of month', function() {
   var component = this.subject( {
-    today: moment("2014-11-26"),
-    monthlyAmount: 1000,
-    includeNextMonth: false
+    startDate: moment("2014-10-31"),
+    monthlyAmount: 1000
   });
 
-  equal(component.get('blurb'), "  **This $1000/month fee is prorated to cover 11/27/2014 - 11/30/2014");
+  equal(component.get('startDateProratedRate'), 1000/31);
+});
+
+test('end date: it calculates prorated rate when end date is the first of the month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-10-01"),
+    endDate: moment("2014-11-01"),
+    monthlyAmount: 1000
+  });
+
+  equal(component.get('endDateProratedRate'), 1000/30);
+});
+
+test('end date: it calculates prorated rate when end date is mid month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-10-01"),
+    endDate: moment("2014-11-15"),
+    monthlyAmount: 1000
+  });
+
+  equal(Math.round(component.get('endDateProratedRate')), 500);
+});
+
+test('end date: it calculates prorated rate when end date is last day of month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-10-31"),
+    monthlyAmount: 1000
+  });
+
+  equal(component.get('endDateProratedRate'), 1000);
+});
+
+test('it calculates the number of middle months when middleMonthCount is greater than one', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-12-15")
+  });
+
+  equal(component.get('middleMonthCount'), 2);
+});
+
+test('it calculates the number of middle months when startDate and endDate are in consecutive months', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-10-15")
+  });
+
+  equal(component.get('middleMonthCount'), 0);
+});
+
+test('it calculates the number of middle months when startDate and endDate are in the same month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-09-15")
+  });
+
+equal(component.get('middleMonthCount'), 0);
+});
+
+test('it calculates the rate properly when the start and end dates are in the same month', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-09-15"),
+    monthlyAmount: 1000
+  });
+
+  equal(Math.round(component.get('proratedRate')), 500);
+});
+
+test('it calculates the rate properly when the start and end dates are one month apart', function() {
+  var component = this.subject( {
+    startDate: moment("2014-08-01"),
+    endDate: moment("2014-09-15"),
+    monthlyAmount: 1000
+  });
+
+  equal(component.get('proratedRate'), 1500);
+});
+
+test('it calculates the rate properly when the start and end dates are more than a month apart', function() {
+  var component = this.subject( {
+    startDate: moment("2014-09-01"),
+    endDate: moment("2014-11-15"),
+    monthlyAmount: 1000
+  });
+
+  equal(component.get('proratedRate'), 2500);
+});
+
+test('the blurb is correct', function() {
+  var component = this.subject( {
+    startDate: moment("2014-11-18"),
+    endDate: moment("2014-11-30"),
+    monthlyAmount: 1000,
+  });
+
+  equal(component.get('blurb'), "  **This $1000/month fee is prorated to cover 11/18/2014 - 11/30/2014");
 });
